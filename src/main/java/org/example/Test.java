a57bc807f781f4f864c09a93b9836efb6a08cef1
Darya Vylomova <qwerty6108@gmail.com>
2023-11-11 17:32:08 +0300

    dont work driver

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -3,12 +3,13 @@ package org.example;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 
-class Test {
+class FormTest {
 
         private WebDriver driver;
 
