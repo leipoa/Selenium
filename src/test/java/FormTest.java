7487d400bc5e3e046130d91bfe1e8dbeca52aea5
Darya Vylomova <qwerty6108@gmail.com>
2023-11-12 20:16:22 +0300

    ci

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -31,7 +31,7 @@ class FormTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:7777/");
+        driver.get("http://localhost:9999/");
 
 
     }
